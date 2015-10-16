8239fff251d9937ef5cc7c21a4f4e04c24554889
Tim Destan <tim@destan.name>
2015-10-15 21:10:39 -0400

    Create enumeration for color.

---
+++ src/main/scala/phelddagrif/colors/Color.scala
@@ -0,0 +1,9 @@
+package phelddagrif.colors
+
+// 105.1. There are five colors in the Magic game: white, blue, black, red, and
+//        green.
+
+object Color extends Enumeration {
+  type Color = Value
+  val White, Blue, Black, Red, Green = Value
+}
