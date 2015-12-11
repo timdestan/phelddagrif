bd4db7dd91c5ee26163044ad00778a0facd9db28
Tim Destan <tim@destan.name>
2015-12-10 23:30:02 -0500

    Add method to combine like mana

--- src/main/scala/phelddagrif/ManaPool.scala
+++ src/main/scala/phelddagrif/ManaPool.scala
@@ -5,10 +5,15 @@ package phelddagrif
 //        stay in the player’s mana pool. Each player’s mana pool empties at the
 //        end of each step and phase.
 
-case class ManaPool(mana: Vector[Mana]) {
-  def add(m: Mana): ManaPool = new ManaPool(m +: mana)
+case class ManaPool(mana: Set[Mana]) {
+  def add(m: Mana): ManaPool = new ManaPool(mana + m)
+
+  def simplify: ManaPool = ManaPool(
+    mana.groupBy { _.manaType }
+      .map { case (typ, counts) => Mana(counts.map(_.amount).sum, typ) }
+      .toSet)
 }
 
 object ManaPool {
-  val empty: ManaPool = new ManaPool(Vector())
+  val empty: ManaPool = new ManaPool(Set())
 }
--- src/test/scala/phelddagrif/ManaPool.scala
+++ src/test/scala/phelddagrif/ManaPool.scala
@@ -1,9 +1,23 @@
 import org.scalatest._
 import phelddagrif._
+import phelddagrif.Mana._ // Implicits
 import phelddagrif.ManaType._
 
 class ManaPoolSpec extends FlatSpec with Matchers {
   "empty mana pool" should "not contain any mana" in {
-    ManaPool.empty.mana.isEmpty should be (true)
+    ManaPool.empty.mana.isEmpty should be(true)
+  }
+
+  "simplify" should "combine like mana types" in {
+    ManaPool.empty.add(1 * Green)
+      .add(2 * Green)
+      .add(3 * Blue)
+      .add(5 * Red)
+      .add(2 * Blue)
+      .simplify should be(
+        ManaPool.empty
+          .add(3 * Green)
+          .add(5 * Blue)
+          .add(5 * Red))
   }
 }
