385a1c2defe5918813904f7306d1b3dacd5ae531
Tim Destan <tim.destan@gmail.com>
2015-12-29 19:45:43 -0500

    Not all mana in costs is the same type.

--- src/main/scala/phelddagrif/Card.scala
+++ src/main/scala/phelddagrif/Card.scala
@@ -2,5 +2,5 @@ package phelddagrif
 
 class Card(val name: String,
            val cardType: Seq[CardType],
-           val manaCost: Mana) {
+           val manaCost: Seq[Mana]) {
 }
--- src/main/scala/phelddagrif/CreatureCard.scala
+++ src/main/scala/phelddagrif/CreatureCard.scala
@@ -1,7 +1,7 @@
 package phelddagrif
 
 class CreatureCard(name: String,
-                   manaCost: Mana,
+                   manaCost: Seq[Mana],
                    val power: Int,
                    val toughness: Int)
   extends Card(name, Seq(CardType.Creature), manaCost)
