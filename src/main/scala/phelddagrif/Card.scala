b43c28923dc5227a503988be26f783c363b052ba
Tim Destan <tim.destan@gmail.com>
2015-12-29 19:43:09 -0500

    Removing another use of enumeration
    It’s cleaner this way.

--- src/main/scala/phelddagrif/Card.scala
+++ src/main/scala/phelddagrif/Card.scala
@@ -1,7 +1,5 @@
 package phelddagrif
 
-import CardType.CardType
-
 class Card(val name: String,
            val cardType: Seq[CardType],
            val manaCost: Mana) {
--- src/main/scala/phelddagrif/CardType.scala
+++ src/main/scala/phelddagrif/CardType.scala
@@ -7,8 +7,15 @@ package phelddagrif
 // We don't cover some of these at the moment, as they only exist in casual
 // game types.
 
-object CardType extends Enumeration {
-  type CardType = Value
-  val Artifact, Creature, Enchantment, Instant,
-      Land, Planeswalker, Sorcery, Tribal = Value
+sealed trait CardType
+
+object CardType {
+  case object Artifact extends CardType
+  case object Creature extends CardType
+  case object Enchantment extends CardType
+  case object Instant extends CardType
+  case object Land extends CardType
+  case object Planeswalker extends CardType
+  case object Sorcery extends CardType
+  case object Tribal extends CardType
 }
