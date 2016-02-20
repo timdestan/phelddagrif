655cbd75f40d6b3373002506acf24386664de099
Tim Destan <tim.destan@gmail.com>
2016-02-20 14:50:29 -0500

    Formatting fix

--- src/main/scala/phelddagrif/Player.scala
+++ src/main/scala/phelddagrif/Player.scala
@@ -1,7 +1,6 @@
 package phelddagrif
 
-case class Player(life: Int,
-  manaPool: ManaPool)
+case class Player(life: Int, manaPool: ManaPool)
 
 object Player {
   def apply(): Player = new Player(20, ManaPool.empty)
