9799335a5b57c2ad8af620ec2fdb0f87756f625e
Tim Destan <tim.destan@gmail.com>
2017-12-18 02:32:50 -0500

    Make Matcher polymorphic.

--- src/main/scala/phelddagrif/search/Matcher.scala
+++ src/main/scala/phelddagrif/search/Matcher.scala
@@ -1,9 +1,9 @@
 package phelddagrif
 package search
 
-case class Pattern[+T](
-    text: String,
-    value: T
+case class Pattern[I, +O](
+    pattern: IndexedSeq[I],
+    value: O
 )
 
 case class Match[+T](
@@ -11,15 +11,16 @@ case class Match[+T](
     start: Int
 )
 
-class Matcher[+T](patterns: List[Pattern[T]]) {
-  def find(text: String): List[Match[T]] =
+class Matcher[-I, +O](patterns: List[Pattern[I, O]]) {
+  def find(input: IndexedSeq[I]): List[Match[O]] =
     for {
       p <- patterns
-      i <- 0 to text.size - p.text.size
-      if (text.slice(i, i + p.text.size) == p.text)
+      i <- 0 to input.size - p.pattern.size
+      if (input.slice(i, i + p.pattern.size) == p.pattern)
     } yield Match(p.value, i)
 }
 
 object Matcher {
-  def apply[T](ps: Pattern[T]*): Matcher[T] = new Matcher[T](ps.toList)
+  def apply[I, O](ps: Pattern[I, O]*): Matcher[I, O] =
+    new Matcher[I, O](ps.toList)
 }
